f5853bb8479fa5a7d803cea8cb0c936786dc8727
김기홍 <68697807+KimGiHong@users.noreply.github.com>
2021-03-15 21:01:03 +0900

    Api integration using useAsync (custom Hook)

--- src/Users.js
+++ src/Users.js
@@ -1,45 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import useAsync from './UseAsync';
+import User from './User';
+
+
+async function getUsers() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+    return response.data;
+}
 
 function Users() {
-    const [users,setUsers] = useState(null);
-    const [loading,setLoading] = useState(false);
-    const [error,setError] = useState(null);
+    const [state, refetch] = useAsync(getUsers, [], true);
+    const [userId, setUserId] = useState(null);
 
-    const fetchUsers = async () => {
-        try {
-            setUsers(null);
-            setError(null);
-            setLoading(true);
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users/'
-            );
-            setUsers(response.data);
-        } catch (e) {
-            setError(e);
-        }
-        setLoading(false);
-    };
-
-    useEffect(() => {
-
-        fetchUsers();
-    }, [])
+    const { loading, data: users, error } = state;
 
     if(loading) return <div>로딩중 ...</div>
     if(error) return <div>에러 발생</div>
-    if(!users) return null;
+    if(!users) return <button onClick={refetch}>불러오기</button>;
 
     return (
         <>
             <ul>
                 {users.map(user => (
-                    <li key={user.id}>
+                    <li key={user.id} onClick={() => setUserId(user.id)} style={{ cursor: 'pointer'}}>
                         {user.username} ({user.name})
                     </li>
                 ))}
             </ul>
-            <button onClick={fetchUsers}>다시 불러오기</button>
+            <button onClick={refetch}>다시 불러오기</button>
+            { userId && <User id={userId} />}
         </>  
     )
 }
