77003dc1e40b1b4e7d73ae0799f38b5e63e2bc47
김기홍 <68697807+KimGiHong@users.noreply.github.com>
2021-03-16 20:19:45 +0900

    Study react-async

--- src/User.js
+++ src/User.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './UseAsync';
+import { useAsync } from 'react-async';
 
-async function getUser(id) {
+async function getUser({id}) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
     );
@@ -10,14 +10,22 @@ async function getUser(id) {
 }
 
 function User({ id }) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  const {
+    data: user,
+    error,
+    isLoading
+  } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
 
-  if (loading) return <div>로딩중...</div>
+  });
+
+  if (isLoading) return <div>로딩중...</div>
   if (error) return <div>에러 발생</div>
   if (!user) return null;
   return (
-    <div>ㅎ
+    <div>
       <h2>{user.username}</h2>
       <p>
         <b>Email: </b> {user.email}
--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from './UseAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 
 
@@ -10,14 +10,14 @@ async function getUsers() {
 }
 
 function Users() {
-    const [state, refetch] = useAsync(getUsers, [], true);
     const [userId, setUserId] = useState(null);
+    const { data: users,error,isLoading,reload,run} = useAsync({
+        deferFn: getUsers
+    })
 
-    const { loading, data: users, error } = state;
-
-    if(loading) return <div>로딩중 ...</div>
+    if(isLoading) return <div>로딩중 ...</div>
     if(error) return <div>에러 발생</div>
-    if(!users) return <button onClick={refetch}>불러오기</button>;
+    if(!users) return <button onClick={run}>불러오기</button>;
 
     return (
         <>
@@ -28,7 +28,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={reload}>다시 불러오기</button>
             { userId && <User id={userId} />}
         </>  
     )
